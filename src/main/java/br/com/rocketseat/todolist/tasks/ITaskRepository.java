b3991b018e4b9a9c4e88838d72f73ad211be7691
Robson <robson.silva@dcx.ufpb.br>
2025-01-10 17:36:30 -0300

    Listando as tasks de cada usuário

--- src/main/java/br/com/rocketseat/todolist/tasks/ITaskRepository.java
+++ src/main/java/br/com/rocketseat/todolist/tasks/ITaskRepository.java
@@ -2,7 +2,10 @@ package br.com.rocketseat.todolist.tasks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByIdUser (UUID idUser);
 }
